741c63807d0d008e09b8b1025df206ca577ac0fc
Jason McDonald <jasonmcd65@gmail.com>
2021-01-08 21:52:29 -0600

    loads notes

--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var path = require("path");
+const fs = require("fs")
 
 var app = express();
 var PORT = 3000;
@@ -16,6 +17,14 @@ app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 })
 
+app.get("/api/notes", function(req, res){
+    console.log('User wants to load notes...')
+    let raw = fs.readFileSync('./db/db.json')
+    let notes = JSON.parse(raw)
+    res.send(notes)
+})
+
+// Server initialization
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
